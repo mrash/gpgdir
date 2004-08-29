b5e2f1d48aaefe49ba52bf9bb73777bd7b5cc816
Michael Rash <mbr@cipherdyne.org>
2004-08-29 18:16:21 +0000

    updated to [+] and related prefixes
    git-svn-id: file:///home/mbr/svn/gpgdir_repos/gpgdir/trunk@70 958e171a-1414-0410-8e2f-9d295d3c0db0

--- install.pl
+++ install.pl
@@ -41,7 +41,7 @@ my $makeCmd = '/usr/bin/make';
 
 ### Everthing after this point must be executed as root.
 $< == 0 && $> == 0 or
-    die " ** You must be root (or equivalent " .
+    die "[*] You must be root (or equivalent " .
         "UID 0 account) to install gpgdir!  Exiting.\n";
 
 my %Cmds = (
@@ -57,41 +57,41 @@ my %Cmds = (
 ### create directories, make sure executables exist, etc.
 &setup();
 
-print " .. Installing gpgdir in $install_dir\n";
+print "[+] Installing gpgdir in $install_dir\n";
 &install_gpgdir();
 
-print " .. Installing GnuPG perl module in $libdir\n";
+print "[+] Installing GnuPG perl module in $libdir\n";
 &install_gnupg_pm();
 
-print " .. Installing Term::Readkey perl module in $libdir\n";
+print "[+] Installing Term::Readkey perl module in $libdir\n";
 &install_term_readkey_pm();
 
-print " .. Installing man page.\n";
+print "[+] Installing man page.\n";
 &install_manpage();
 
-print " .. gpgdir installed!\n";
+print "[+] gpgdir installed!\n";
 
 exit 0;
 #===================== end main =======================
 
 sub install_gpgdir() {
-    die " ** gpgdir does not exist.  Download gpgdir from " .
+    die "[*] gpgdir does not exist.  Download gpgdir from " .
         "http://www.cipherdyne.org/gpgdir" unless -e 'gpgdir';
-    copy 'gpgdir', "${install_dir}/gpgdir" or die " ** Could not copy " .
+    copy 'gpgdir', "${install_dir}/gpgdir" or die "[*] Could not copy " .
         "gpgdir to $install_dir: $!";
-    chmod 0755, "${install_dir}/gpgdir" or die " ** Could not set " .
+    chmod 0755, "${install_dir}/gpgdir" or die "[*] Could not set " .
         "permissions on gpgdir to 0755";
     chown 0, 0, "${install_dir}/gpgdir" or
-        die " ** Could not chown 0,0,${install_dir}/gpgdir: $!";
+        die "[*] Could not chown 0,0,${install_dir}/gpgdir: $!";
     return;
 }
 
 sub install_gnupg_pm() {
     ### installing GnuPG
-    chdir 'GnuPG' or die " ** Could not chdir to ",
+    chdir 'GnuPG' or die "[*] Could not chdir to ",
         "GnuPG: $!";
     unless (-e 'Makefile.PL' && -e 'GnuPG.pm') {
-        die " ** Your source directory appears to be incomplete!  GnuPG.pm ",
+        die "[*] Your source directory appears to be incomplete!  GnuPG.pm ",
             "is missing.\n    Download the latest sources from ",
             "http://www.cipherdyne.org\n";
     }
@@ -105,10 +105,10 @@ sub install_gnupg_pm() {
 
 sub install_term_readkey_pm() {
     ### installing GnuPG
-    chdir 'TermReadKey' or die " ** Could not chdir to ",
+    chdir 'TermReadKey' or die "[*] Could not chdir to ",
         "TermReadKey: $!";
     unless (-e 'Makefile.PL' && -e 'ReadKey.pm') {
-        die " ** Your source directory appears to be incomplete!  ReadKey.pm ",
+        die "[*] Your source directory appears to be incomplete!  ReadKey.pm ",
             "is missing.\n    Download the latest sources from ",
             "http://www.cipherdyne.org\n";
     }
@@ -121,7 +121,7 @@ sub install_term_readkey_pm() {
 }
 
 sub install_manpage() {
-    die " ** man page: $manpage does not exist.  Download gpgdir " .
+    die "[*] man page: $manpage does not exist.  Download gpgdir " .
         "from http://www.cipherdyne.org/gpgdir" unless -e $manpage;
     ### default location to put the gpgdir man page, but check with
     ### /etc/man.config
@@ -130,7 +130,7 @@ sub install_manpage() {
         ### prefer to install $manpage in /usr/local/man/man1 if
         ### this directory is configured in /etc/man.config
         open M, '< /etc/man.config' or
-            die " ** Could not open /etc/man.config: $!";
+            die "[*] Could not open /etc/man.config: $!";
         my @lines = <M>;
         close M;
         ### prefer the path "/usr/share/man"
@@ -167,14 +167,14 @@ sub install_manpage() {
     }
     mkdir $mpath, 0755 unless -d $mpath;
     my $mfile = "${mpath}/${manpage}";
-    print " .. Installing $manpage man page as: $mfile\n";
-    copy $manpage, $mfile or die " ** Could not copy $manpage to " .
+    print "[+] Installing $manpage man page as: $mfile\n";
+    copy $manpage, $mfile or die "[*] Could not copy $manpage to " .
         "$mfile: $!";
-    chmod 0644, $mfile or die " ** Could not set permissions on ".
+    chmod 0644, $mfile or die "[*] Could not set permissions on ".
         "$mfile to 0644";
     chown 0, 0, $mfile or
-        die " ** Could not chown 0,0,$mfile: $!";
-    print " .. Compressing man page: $mfile\n";
+        die "[*] Could not chown 0,0,$mfile: $!";
+    print "[+] Compressing man page: $mfile\n";
     ### remove the old one so gzip doesn't prompt us
     unlink "${mfile}.gz" if -e "${mfile}.gz";
     system "$Cmds{'gzip'} $mfile";
@@ -202,13 +202,13 @@ sub check_commands() {
                 }
             }
             unless ($found) {
-                die "\n ** Could not find $cmd anywhere!!!  ",
+                die "[*] Could not find $cmd anywhere!!!  ",
                     "Please edit the config section to include the path to ",
                     "$cmd.\n";
             }
         }
         unless (-x $Cmds{$cmd}) {
-            die "\n ** $cmd is located at ",
+            die "[*] $cmd is located at ",
                 "$Cmds{$cmd} but is not executable by uid: $<\n";
         }
     }
@@ -217,7 +217,7 @@ sub check_commands() {
 
 sub setup() {
     unless (-d $libdir) {
-        mkdir $libdir, 0755 or die " ** Could not create $libdir: $!"
+        mkdir $libdir, 0755 or die "[*] Could not create $libdir: $!"
     }
     return;
 }
